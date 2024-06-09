88d4d41563422c4ba2e2ed08ac9e81d7b0735675
drclcomputers <132164125+drclcomputers@users.noreply.github.com>
2024-06-09 17:52:29 +0300

    Add project files.

---
+++ password_generator/password_generator.cpp
@@ -0,0 +1,22 @@
+#include<fstream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+ofstream file("password.txt");
+
+int passgen() {
+	return rand() % 94 + 32;
+}
+
+void put_in_file(char x) {
+	file << (char)x;
+}
+
+int main() {
+	srand(time(NULL));
+	for (int i = 1; i <= 20; i++) {
+		put_in_file(passgen());
+	}
+	return 0;
+}
